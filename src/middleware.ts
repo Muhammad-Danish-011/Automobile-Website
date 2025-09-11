679583db970870fe814ed08ad95b8f05d5a8640b
Muhammad-Danish-011 <softwareengineer.danish@gmail.com>
2025-09-11 14:59:41 +0500

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,10 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const auth = req.cookies.get("auth");
 
-  // Agar user dashboard pe jaye aur login na ho to redirect
   if (req.nextUrl.pathname.startsWith("/dashboard") && !auth) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // Agar user login page pe jaye aur already login ho
   if (req.nextUrl.pathname.startsWith("/login") && auth) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
@@ -18,5 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"], // kin routes pe middleware chale
+  matcher: ["/dashboard/:path*", "/login"],
 };
